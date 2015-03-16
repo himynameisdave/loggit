8ecaa0efbfa89f55ccd1f1dd6e47a706640a50cd
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-03-16 10:18:53 -0400

    throws error if no msg is passed

--- loggit.js
+++ loggit.js
@@ -1,49 +1,47 @@
 var chalk = require('chalk'),
     loggit = function(msg, color, symbol) {
 
-  //  Our variables for the module
-  var col       = "yellow",
-      sym       = "*",
-      openClose = "",
-      size      = 46; // total size of the symbol string
+      if(typeof msg  === "undefined"){
+        console.log(chalk.red( "  !!!   !!!   !!!ERROR!!!   !!!   !!!   \nYou must supply a message to be logged!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" ));
+        return null;
+      }
 
-  // Applies passed-in color if it exists
-  if(typeof color  != "undefined"){
-    col = color;
-  }
-  // If the symbol was provided, this ensures that it's only 1 character long
-  if(typeof symbol != "undefined"){
-    if(symbol.length > 2){
-      sym = symbol.slice(0,2);
-    }else{
-      sym = symbol;
-    }
-  }
-  // Symbol adder
-  if(sym.length === 1){
-    for( i=0; i<=size; i++ ){
-      openClose += sym;
-    }
-  }
-  if(sym.length === 2){
-    for( i=0; i<=(size/2); i++ ){
-      openClose += sym;
-    }
-  }
-  //  wut wut
-  // if(msg.length >= size){
-  //   var m1 = msg.slice(0, size-2),
-  //       m2 = msg.slice(size-2, msg.length);
+      //  Our variables for the module
+      var col       = "yellow",
+          sym       = "*",
+          openClose = "",
+          size      = 46; // total size of the symbol string
 
-  //   msg = m1 + '\n' + m2;
-  // }
+      // Applies passed-in color if it exists
+      if(typeof color  !== "undefined"){
+        col = color;
+      }
+      // If the symbol was provided, this ensures that it's only 1 character long
+      if(typeof symbol !== "undefined"){
+        if(symbol.length > 2){
+          sym = symbol.slice(0,2);
+        }else{
+          sym = symbol;
+        }
+      }
+      // Symbol adder
+      if(sym.length === 1){
+        for( i=0; i<=size; i++ ){
+          openClose += sym;
+        }
+      }
+      if(sym.length === 2){
+        for( i=0; i<=(size/2); i++ ){
+          openClose += sym;
+        }
+      }
 
-  //  what to actually print
-  var printThis = openClose + "\n" + msg + "\n" + openClose;
+      //  what to actually print
+      var printThis = openClose + "\n" + msg + "\n" + openClose;
 
-  //  do it
-  console.log(chalk[col]( printThis ));
+      //  do it
+      console.log(chalk[col]( printThis ));
 
-};
+    };
 
 module.exports = loggit;
